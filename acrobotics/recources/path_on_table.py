8fe9013f71576e4e905df44fa43d0607991fadbe
jeroendm <jeroendemaeyer@live.be>
2019-04-30 10:15:18 +0200

    clean up example

---
+++ acrobotics/recources/path_on_table.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import numpy as np
+from acrobotics.path import TolPositionPoint, TolerancedNumber
+from acrobotics.path import FreeOrientationPt
+from acrobotics.geometry import Shape, Collection
+from pyquaternion import Quaternion
+
+
+# sinusoid line with pos tolerance along x
+# end-effector pointing down with fixed orientation
+q1 = Quaternion(axis=[0, 1, 0], angle=np.pi)
+dx = 0.05
+
+path_pos_tol = []
+for s in np.linspace(0, 1, 10):
+    xi = 0.8 + 0.1 * np.sin(2 * np.pi * s)
+    xt = TolerancedNumber(xi - dx, xi + dx)
+    yi = s * 0.2 + (1-s) * (-0.2)
+    zi = 0.2
+    path_pos_tol.append(TolPositionPoint([xt, yi, zi], q1))
+
+# straight line with free end-effector orientation
+path_ori_free = []
+for s in np.linspace(0, 1, 8):
+    xi = 0.8
+    yi = s * 0.2 + (1-s) * (-0.2)
+    zi = 0.2
+    path_ori_free.append(FreeOrientationPt([xi, yi, zi]))
+
+# table with obstacle
+table = Shape(0.5, 0.5, 0.1)
+table_tf = np.array([[1, 0, 0, 0.80],
+                    [0, 1, 0, 0.00],
+                    [0, 0, 1, 0.12],
+                    [0, 0, 0, 1]])
+
+obstacle = Shape(0.1, 0.1, 0.5)
+obstacle_tf = np.array([[1, 0, 0, 1.00],
+                        [0, 1, 0, 0.25],
+                        [0, 0, 1, 0.12],
+                        [0, 0, 0, 1]])
+
+scene = Collection([table, obstacle],
+                   [table_tf, obstacle_tf])
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from acrobotics.util import get_default_axes3d,plot_reference_frame
+
+    fig2, ax2 = get_default_axes3d([-1, 1], [-1, 1], [-1, 1])
+    for pi in path_ori_free: pi.plot(ax2)
+    scene.plot(ax2, c='g')
+    plt.show()
--- examples/iterative_grid_refining.py
+++ examples/iterative_grid_refining.py
@@ -1,50 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
-import matplotlib.pyplot as plt
-from acrobotics.util import get_default_axes3d
 from acrobotics.recources.robots import Kuka
-from acrobotics.path import FreeOrientationPt
-from acrobotics.geometry import Shape, Collection
+from acrobotics.recources.path_on_table import path_pos_tol, scene, path_ori_free
+from pyquaternion import Quaternion
 
 robot = Kuka()
+path = path_pos_tol
+# path = path_ori_free
 
-path = []
-for s in np.linspace(0, 1, 8):
-    xi = 0.8
-    yi = s * 0.2 + (1-s) * (-0.2)
-    zi = 0.2
-    path.append(FreeOrientationPt([xi, yi, zi]))
-
-floor_plane = Shape(0.5, 0.5, 0.1)
-floor_plane_tf = np.array([[1, 0, 0, 0.80],
-                            [0, 1, 0, 0.00],
-                            [0, 0, 1, 0.12],
-                            [0, 0, 0, 1]])
-
-scene = Collection([floor_plane], [floor_plane_tf])
-
-#%% PLAN PATH
 from acrobotics.planning import cart_to_joint_iterative
 
-res = cart_to_joint_iterative(robot, path, scene, num_samples=200, max_iters=3)
+res = cart_to_joint_iterative(robot, path, scene, num_samples=200, max_iters=5)
 print(res)
 qp_sol = res['path']
 
+
+import matplotlib.pyplot as plt
+from acrobotics.util import get_default_axes3d,plot_reference_frame
+
 fig, ax = plt.subplots()
 ax.plot(res['costs'], 'o-')
+ax.set_title('Cost as function of iterations')
 
-#%% RUN for different amount of sampels
-# solutions = []
-# for ns in [100, 200, 500, 1000]:
-#     print('Running planner for ns = {}.'.format(ns))
-#     Q = cart_to_joint_no_redundancy(robot, path, scene, num_samples=ns)
-#     res = get_shortest_path(Q, method='dijkstra')
-#     solutions.append(res)
-
-#%% ANIMATE
-import matplotlib.pyplot as plt
-fig2, ax2 = get_default_axes3d([-1, 1], [-1, 1], [-1, 1])
+fig2, ax2 = get_default_axes3d([0, 1], [-0.5, 0.5], [0, 1])
 for pi in path: pi.plot(ax2)
 scene.plot(ax2, c='g')
 robot.animate_path(fig2, ax2, qp_sol)
